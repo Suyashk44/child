84812895ebb112697d610c990055331c279d4c1f
Priyanka <shindepriyanka564@gmail.com>
2020-07-12 19:06:44 +0530

    As per requirement arrow function applied and CSS styling is placed in external folder i.e src/index.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-	return <div className="App">Child</div>;
+import React from "react"
+
+class App extends React.Component {
+constructor () {
+    super () 
+    this.state = {
+        number : "0"
+    }
+    // this.handleIncr = this.handleIncr.bind(this)
+    // this.handleDecre = this.handleDecre.bind(this)
+    // this.handleReset = this.handleReset.bind(this)
+}
+// all three method changed using arrow function
+handleIncr =()=> {
+   return (
+    this.setState (
+        function (prevState){
+          return  {number : prevState.number ++}
+           }
+   )
+   )
+}
+handleDecre =()=>  {
+    return (
+        this.setState (
+         function (prevState){
+           return  {number : prevState.number --}
+            }
+    )
+    )
+}
+handleReset =()=>  {
+    return (
+        this.setState (
+        function (prevState){
+          return  {number : 0}
+           }
+    )
+   )
+} 
+
+render () {
+return (
+    <div className = "CounterDisplay">
+        <h1>Counter : {this.state.number}</h1> 
+        <button onClick = {this.handleIncr}>Increment</button>
+        <button onClick = {this.handleReset}>Reset</button>
+        <button onClick = {this.handleDecre}>Decrement</button>
+    </div>
+)
 }
 
-export default App;
+}
+
+export default App
