3314e796e6fddad89e3dc9d566f038cf3afb304a
Priyanka <shindepriyanka564@gmail.com>
2020-07-13 10:54:01 +0530

    fixed handleDecr and handleReset

--- src/App.js
+++ src/App.js
@@ -1,56 +1,41 @@
 import React from 'react';
 import './App.css';
 
-import React from "react"
-
 class App extends React.Component {
 constructor () {
     super () 
     this.state = {
-        number : "0"
-    }
+        number : 0
+    };
     // this.handleIncr = this.handleIncr.bind(this)
     // this.handleDecre = this.handleDecre.bind(this)
     // this.handleReset = this.handleReset.bind(this)
-}
+};
 // all three method changed using arrow function
-handleIncr =()=> {
-   return (
-    this.setState (
-        function (prevState){
-          return  {number : prevState.number ++}
-           }
-   )
-   )
-}
-handleDecre =()=>  {
-    return (
-        this.setState (
+handleIncr = () => { this.setState (function (prevState){
+            return  {number : prevState.number + 1 };
+           });
+};
+handleDecre = () =>  { this.setState (
          function (prevState){
-           return  {number : prevState.number --}
-            }
-    )
-    )
-}
-handleReset =()=>  {
-    return (
-        this.setState (
+           return  {number : prevState.number - 1 };
+            });
+};
+handleReset = () =>  {this.setState (
         function (prevState){
-          return  {number : 0}
-           }
-    )
-   )
-} 
+          return  {number : 0};
+           });
+};
 
 render () {
 return (
-    <div className = "CounterDisplay">
-        <h1>Counter : {this.state.number}</h1> 
+    <div className = "Counter-Display">;
+        <h1>Counter : {this.state.number}</h1>
         <button onClick = {this.handleIncr}>Increment</button>
         <button onClick = {this.handleReset}>Reset</button>
         <button onClick = {this.handleDecre}>Decrement</button>
     </div>
-)
+);
 }
 
 }
