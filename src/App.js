89e4143b765da405a689bebf99b6457b81070358
Priyanka <shindepriyanka564@gmail.com>
2020-07-14 10:31:49 +0530

    removed unnecessary code and formated code

--- src/App.js
+++ src/App.js
@@ -8,36 +8,36 @@ class App extends React.Component {
     this.state = {
       number: 0,
     };
-    // this.handleIncr = this.handleIncr.bind(this)
-    // this.handleDecre = this.handleDecre.bind(this)
-    // this.handleReset = this.handleReset.bind(this)
-};
-// all three method changed using arrow function
-handleIncr = () => { this.setState (function (prevState){
-            return  {number : prevState.number + 1 };
-           });
-};
-handleDecre = () =>  { this.setState (
-         function (prevState){
-           return  {number : prevState.number - 1 };
-            });
-};
-handleReset = () =>  {this.setState (
-        function (prevState){
-          return  {number : 0};
-           });
-};
+  };
+ 
+  handleIncr = () => {
+    this.setState(function (prevState) {
+      return { number: prevState.number + 1 };
+    });
+  };
+  handleDecre = () => {
+      this.setState(
+      function (prevState) {
+        return { number: prevState.number - 1 };
+      });
+  };
+  handleReset = () => {
+    this.setState(
+       function (prevState) {
+          return { number: 0 };
+      });
+  };
 
-render () {
-return (
-    <div className = "Counter-Display">;
+  render() {
+    return (
+      <div className="Counter-Display">
         <h1>Counter : {this.state.number}</h1>
-        <button onClick = {this.handleIncr}>Increment</button>
-        <button onClick = {this.handleReset}>Reset</button>
-        <button onClick = {this.handleDecre}>Decrement</button>
-    </div>
-);
-}
+        <button onClick={this.handleIncr} >Add</button>
+        <button onClick={this.handleReset} >Reset</button>
+        <button onClick={this.handleDecre} >Decr</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
