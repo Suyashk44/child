68f2f7e5a36ab9f67f5846e2e5bad2c15d49ce49
Priyanka <shindepriyanka564@gmail.com>
2020-07-15 18:48:29 +0530

    setting max count value to 50

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import "./App.css";
 
@@ -9,35 +8,45 @@ class App extends React.Component {
       number: 0,
     };
   };
- 
+//////////////////////////////////////////using For loop here////////////////////////////////////////////////////////////////////////
   handleIncr = () => {
     this.setState(function (prevState) {
-      return { number: prevState.number + 1 };
+      for (this.state.number = 0; this.state.number <= 50; this.state.number++) {
+        return { number: prevState.number + 1 };
+      };
     });
   };
+
+  /////////////////////////////////////////////////using if else condition////////////////////////////////////////////////////
+// handleIncr = () => {
+//   if (this.state.number <= 50){
+//     alert ("You have reached to the max no")
+//     this.setState(function () {return{number: 0}});
+//   }else {
+//     this.setState(function (prevState) {return {number:prevState.number+1}});
+//   };
+// };
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   handleDecre = () => {
-      this.setState(
-      function (prevState) {
-        return { number: prevState.number - 1 };
-      });
+    this.setState(function (prevState) {return { number: prevState.number - 1 };});
   };
   handleReset = () => {
-    this.setState(
-       function (prevState) {
-          return { number: 0 };
-      });
+    this.setState(function (prevState) {
+      return { number: 0 };
+    });
   };
 
   render() {
     return (
       <div className="Counter-Display">
         <h1>Counter : {this.state.number}</h1>
-        <button onClick={this.handleIncr} >Add</button>
-        <button onClick={this.handleReset} >Reset</button>
-        <button onClick={this.handleDecre} >Decr</button>
+        <button onClick={this.handleIncr}>Add</button>
+        <button onClick={this.handleReset}>Reset</button>
+        <button onClick={this.handleDecre}>Decr</button>
       </div>
     );
-  }
-}
+  };
+};
 
-export default App
+export default App;
